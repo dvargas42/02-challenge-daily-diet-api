7bcf6e89b182d1245d64cea439544b3f745b9dc0
dvargas42 <dvargas.eng@gmail.com>
2025-07-06 00:20:51 -0300

    refactor: :fire: remove useless files

--- src/routes/schemas/user-login.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-const userLoginSchema = z.object({
-  email: z.string().email('Email is invalid'),
-  password: z.string(),
-})
-
-export const userLoginValidation = userLoginSchema.parse
